c5fe28dc3744626d24e43fa5188395762a9fd024
Roland Singer <roland@desertbit.com>
2019-01-11 04:01:59 +0100

    further progress

---
+++ internal/binding/headers/object.h
@@ -0,0 +1,20 @@
+/*
+ *  GML - Go QML
+ *  Copyright (c) 2019 Roland Singer [roland.singer@deserbit.com]
+ *  Copyright (c) 2019 Sebastian Borchers [sebastian@deserbit.com]
+ */
+
+#ifndef GML_HEADER_OBJECT_H
+#define GML_HEADER_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void* gml_object;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
