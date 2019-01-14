f336b8b15ab17ed641cd4e3783a66a6cde9680a6
Roland Singer <roland@desertbit.com>
2019-01-14 13:34:34 +0100

    further progress

--- internal/binding/headers/gml_error.h
+++ internal/binding/headers/gml_error.h
@@ -25,31 +25,14 @@
  * SOFTWARE.
  */
 
-#ifndef GML_HEADER_APP_H
-#define GML_HEADER_APP_H
+#ifndef GML_HEADER_ERROR_H
+#define GML_HEADER_ERROR_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "gml_object.h"
-
-typedef void* gml_app;
-
-typedef void (*gml_app_run_main_cb_t)(void* go_ptr);
-void gml_app_run_main_cb_register(gml_app_run_main_cb_t cb);
-
-gml_app gml_app_new (int argv, char** argc);
-void    gml_app_free(gml_app app);
-int     gml_app_exec(gml_app app);
-int     gml_app_quit(gml_app app);
-int     gml_app_run_main(gml_app app, void* go_ptr);
-
-int     gml_app_load     (gml_app app, const char* url);
-int     gml_app_load_data(gml_app app, const char* data);
-int     gml_app_add_import_path(gml_app app, const char* path);
-
-int     gml_app_set_root_context_property(gml_app app, const char* name, gml_object obj);
+typedef void* gml_object;
 
 #ifdef __cplusplus
 }
